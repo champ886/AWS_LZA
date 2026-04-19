2cb73331d4da55d3a477e05a443bee94a94067b5
champion <champion_nweke@yahoo.com>
2026-04-19 12:35:08 +1000

    backend config

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-landing-zone"
+    bucket         = "tf-state-landing-zone-champ-001"
     key            = "dev/terraform.tfstate"
     region         = "ap-southeast-2"
     dynamodb_table = "tf-locks"
