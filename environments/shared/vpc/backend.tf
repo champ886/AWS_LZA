198f07bcd4f89e1c36ae2accadbc9610b97f38c1
champion <champion_nweke@yahoo.com>
2026-04-29 23:39:50 +1000

    Create a shared security vpc between for both prod and dev and removed indivdual in VPC's in same accout model

--- environments/shared/vpc/backend.tf
+++ environments/shared/vpc/backend.tf
@@ -8,7 +8,7 @@ terraform {
     bucket         = "tf-state-landing-zone-champ-001"
     key            = "aws-lza/shared/vpc/terraform.tfstate"
     region         = "ap-southeast-2"
-    dynamodb_table = "tf-lock"
+    dynamodb_table = "tf-locks"
     encrypt        = true
   }
 }
